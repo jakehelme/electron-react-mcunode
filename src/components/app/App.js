6a8b69a5962ca7a5e1de688220766d95991cf406
Jake Helme <jake.helme@gmail.com>
2017-09-02 01:35:36 +0200

    Started laying out a bit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Icon } from 'react-materialize';
+import { ChromePicker } from 'react-color';
 import 'materialize-css/dist/css/materialize.css';
 import 'materialize-css/dist/js/materialize.js';
 import './App.css';
@@ -7,9 +8,12 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Button waves='light'>
-					<Icon>thumb_up</Icon>
+			<div id="app">
+				<div id="picker-container">
+					<ChromePicker disableAlpha={true} />
+				</div>
+				<Button waves='light'>hit it!
+					<Icon right>send</Icon>
 				</Button>
 			</div>
 		);
