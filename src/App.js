8938e1eeaf2afa9b8f873f99ace533e6249bab80
Jake Helme <jake.helme@gmail.com>
2017-09-02 00:19:38 +0200

    Ejected create react app to be able to import materialize correctly as I wanted it to be bundled so the app can work without internet connection.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import { Button, Icon } from 'react-materialize'
-import logo from './logo.svg';
+import { Button, Icon } from 'react-materialize';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize.js';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h2>Welcome to React</h2>
-				</div>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
 				<Button waves='light'>
 					<Icon>thumb_up</Icon>
 				</Button>
