617e996e9705c5a11fdfb652691cf10b5f7a064e
Jake Helme <jake.helme@gmail.com>
2017-09-02 14:28:27 +0200

    Removed foreman, wasn't working for me

--- src/startup/electron-starter.js
+++ src/startup/electron-starter.js
@@ -1,25 +1,32 @@
+/* eslint no-console: "off" */
 const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
-// const path = require('path');
-// const url = require('url');
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
 	// Create the browser window.
-	mainWindow = new BrowserWindow({width: 800, height: 600});
+	mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
 	// and load the index.html of the app.
-	mainWindow.loadURL('http://localhost:3000');
+	const startUrl = process.env.ELECTRON_START_URL || url.format({
+		pathname: path.join(__dirname, './../../build/index.html'),
+		protocol: 'file:',
+		slashes: true
+	});
+
+	mainWindow.loadURL(startUrl);
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools();
+	// mainWindow.webContents.openDevTools();
 
 	// Emitted when the window is closed.
 	mainWindow.on('closed', function () {
